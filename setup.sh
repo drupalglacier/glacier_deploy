6416fd0e154f201b084cfa55a52f43435e8db8c9
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-10-25 09:41:41 +0100

    added TODOs

--- setup.sh
+++ setup.sh
@@ -39,3 +39,11 @@ fi
 drush -y make $LOCAL_MAKE_FILE $LOCAL_DOCROOT/tmp
 rsync -av $LOCAL_DOCROOT/tmp/ $LOCAL_DOCROOT
 rm -Rf $LOCAL_DOCROOT/tmp
+
+
+
+# TODO: Create settings.local.php for the current environemnt and include it in the regular settings.php
+# TODO: Install Drupal
+# TODO: Install Modules
+# TODO: create sub theme, admin sub theme and deployment helper module
+# TODO: settings
