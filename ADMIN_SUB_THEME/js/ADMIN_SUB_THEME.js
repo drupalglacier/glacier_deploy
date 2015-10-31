529f95021f2993c50b99aaaee532c1992bfb6bb3
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-10-31 15:54:38 +0100

    added files for the admin sub theme

---
+++ ADMIN_SUB_THEME/js/ADMIN_SUB_THEME.js
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * ADMIN_SUB_THEME JavaScript code.
+ */
+
+// (function ($) {
+//   Drupal.behaviors.ADMIN_SUB_THEME = {
+//     attach: function (context, settings) {
+//     }
+//   };
+// }(jQuery));
