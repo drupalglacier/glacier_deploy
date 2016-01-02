5392dcc166ce8b81d3c791f685033973ea57541d
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-01-02 14:20:58 +0100

    removed "notify" option from the drush site-install command call, because it does not what i thought it does and the command fails with this option added

--- setup.sh
+++ setup.sh
@@ -165,7 +165,7 @@ mkdir -p "$WORKSPACE/all/profiles"
 ( cd "$WORKSPACE/all/profiles" && git clone https://github.com/drupalglacier/glacier_profile.git )
 
 # Install Drupal.
-( cd "$DOCROOT" && drush -y site-install glacier_profile --account-mail="$ACCOUNT_MAIL" --account-name="$ACCOUNT_NAME" --db-url="$DATABASE_DRIVER://$DATABASE_USERNAME:$DATABASE_PASSWORD@$DATABASE_HOST/$DATABASE" --site-name="$PROJECT_HUMAN_READABLE_NAME" --notify )
+( cd "$DOCROOT" && drush -y site-install glacier_profile --account-mail="$ACCOUNT_MAIL" --account-name="$ACCOUNT_NAME" --db-url="$DATABASE_DRIVER://$DATABASE_USERNAME:$DATABASE_PASSWORD@$DATABASE_HOST/$DATABASE" --site-name="$PROJECT_HUMAN_READABLE_NAME" )
 
 
 
